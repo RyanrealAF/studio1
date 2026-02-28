d26f61cbfe291bf5711b073ee1db7da820b5ab6f
Syntax Shaman <syntaxshaman@gmail.com>
2026-02-28 15:49:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1104871502-cfb0a",
+  "appId": "1:712466461238:web:d7563722aba18e6fed9e97",
+  "apiKey": "AIzaSyCHnAXeyBaHpW4vvh2_yrFmXZrFeO56_gQ",
+  "authDomain": "studio-1104871502-cfb0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "712466461238"
+};
